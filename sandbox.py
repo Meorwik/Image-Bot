2435240834b1817d1204780e5d772cbf82e4eb22
Meorwik <ilapolienko77@gmail.com>
2022-10-30 00:59:07 +0600

    make it magically work

--- sandbox.py
+++ sandbox.py
@@ -1,29 +1,43 @@
 from string import ascii_lowercase
 
-
-class CeasarDecoder:
+class CaesarDecoder:
     def __init__(self):
-        self.output = ''
-        self.key = 0
-    
-    def code_phrase(self, phrase: str, key: int):
-        for letter in phrase:
-            letter_index = ascii_lowercase.find(letter)
-            coded_letter_index = key + letter_index
-            output += ascii_lowercase[coded_letter_index]
-            return output
-
-    def set_key(self, new_key: int):
-        self.key = new_key
+        self.__output: str = ''
+        self.__key: int = 3
 
     def get_key(self):
-        return self.key
+        return self.__key
     
-    def set_output(self, output: str):
-        pass
-    def decode_phrase(self, phrase: str, key: int):
-        self.code_phrase(phrase=phrase, key=-key)
-
-
-    def get_user_input(self):
-        pass
+    def __set_output(self, output: str):
+        self.__output = output
+    
+    def get_output(self):
+        return self.__output
+    
+    def code_phrase(self, phrase: str, *key: int):
+        def letter_cycle(key: int):
+            output: str = ""
+            for letter in phrase:
+                letter_index = ascii_lowercase.find(letter)
+                coded_letter_index = letter_index + key
+                output += ascii_lowercase[coded_letter_index]
+            self.__set_output(output=output)
+            return self.get_output()
+                
+        if key:
+            users_key = key[0]
+            letter_cycle(users_key)
+        else:
+            default_key = self.get_key()
+            letter_cycle(default_key)
+            
+    
+    def decode_phrase(self, phrase: str, *key: int):
+        try:
+            self.code_phrase(phrase, key[0])
+        except IndexError:
+            self.code_phrase(phrase, -self.get_key())
+        return self.get_output()
+    
+a = CaesarDecoder()
+print(a.code_phrase(phrase="hello"))
