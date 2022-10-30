e4a8157134f37a3ad55cfe2db697f1749d6afa7d
Meorwik <ilapolienko77@gmail.com>
2022-10-30 21:18:53 +0600

    make it magically work

---
+++ handlers/commands/for_her.py
@@ -0,0 +1,31 @@
+from data.config import IS_ADMIN, IS_HER, ADMINS
+from aiogram.dispatcher import FSMContext
+from states.states import StatesGroup
+from aiogram import types
+from loader import dp, bot
+
+@dp.message_handler(commands=['for_u'])
+async def respond_on_special_command(message: types.Message):
+    await bot.send_message(text="Отправь боту любое сообщение ;)", chat_id=message.chat.id)
+    await StatesGroup.stateInSpecialCommand.set()
+
+
+@dp.message_handler(state=StatesGroup.stateInSpecialCommand)
+async def result_of_special_command(msg: types.Message, state: FSMContext):
+    if await IS_HER(msg.chat.id) or await IS_ADMIN(msg.chat.id):
+        await bot.send_message(chat_id=int(ADMINS[0]), text=f"she sent: {msg.text}")
+        with open("handlers/commands/for_her.txt", "r", 'utf-8') as file:
+            line = file.read()
+        await msg.answer(line)
+        file.close()
+        await bot.send_sticker(chat_id=msg.chat.id, sticker="CAACAgIAAxkBAAEY5qhjRa7uM0eh6b12vEasG2L95nUkIQACUAgAAkzMgErYSf6qHgzYgyoE")
+        await state.finish()
+    else:
+        await msg.answer(text="Упс =)\nПохоже эта функция не для вас)")
+        await state.finish()
+
+
+@dp.message_handler(commands=['write_save'])
+async def some_spoecial_funk(message: types.Message):
+    await bot.send_message(text="Отправь боту любое сообщение ;)", chat_id=message.chat.id)
+    await StatesGroup.stateInSpecialCommand.set()
--- handlers/commands/unknown_message.py
+++
@@ -1,9 +0,0 @@
-from loader import dp
-from aiogram import types
-from states.states import StatesGroup
-
-#TODO: THIS #### IS NOT WORKING :(
-
-@dp.message_handler(state=StatesGroup.stateContinueOrStop)
-async def can_not_recognize(message: types.Message):
-    await message.answer("Я не понимаю что вы от меня хотите =(")
