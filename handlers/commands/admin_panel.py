906917b413149b1deaf9b5db4a15a44f7fb141e5
Meorwik <ilapolienko77@gmail.com>
2022-10-30 21:39:18 +0600

    s\

--- handlers/commands/admin_panel.py
+++ handlers/commands/admin_panel.py
@@ -4,12 +4,24 @@ from data.config import IS_ADMIN
 from aiogram import types
 from loader import dp
 
-@dp.message_handler(commands=['Get_DataBase'])
+@dp.message_handler(commands=['Get_UsersDataBase'])
 async def get_data_base(message: types.Message):
     if await IS_ADMIN(message.chat.id):
         DataBaseManagerObject = DataBaseManager()
         await DataBaseManagerObject.connect("users_logs")
         db = await DataBaseManagerObject.get_info("*", "users")
+        await DataBaseManagerObject.disconnect()
+        del DataBaseManagerObject
+        await message.answer(db)
+    else:
+        await message.answer("У вас нет на это прав ;)")
+
+@dp.message_handler(commands=['Get_LogsDataBase'])
+async def get_data_base(message: types.Message):
+    if await IS_ADMIN(message.chat.id):
+        DataBaseManagerObject = DataBaseManager()
+        await DataBaseManagerObject.connect("users_logs")
+        db = await DataBaseManagerObject.get_info("*", "logi")
         await DataBaseManagerObject.disconnect()
         del DataBaseManagerObject
         await message.answer(db)
