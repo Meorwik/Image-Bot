e5e3749884cfce8138329532c4874942fe68d900
Meorwik <ilapolienko77@gmail.com>
2022-10-30 22:56:18 +0600

    again

--- handlers/commands/admin_panel.py
+++ handlers/commands/admin_panel.py
@@ -9,21 +9,23 @@ from loader import dp
 async def switch_admin_mode(message: types.Message):
     if await IS_ADMIN(message.chat.id):
         await StatesGroup.stateAdminMode.set()
+        await message.answer("adminModeActiveted")
     else: 
         await message.answer("У вас нет на это прав )")
         
 @dp.message_handler(commands=['Get_UsersDataBase'], state=StatesGroup.stateAdminMode)
 async def get_users_data_base(message: types.Message):
-    DataBaseInfo = getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="users")
+    DataBaseInfo = await getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="users")
     await message.answer(DataBaseInfo)
 
 
 @dp.message_handler(commands=['Get_LogsDataBase'], state=StatesGroup.stateAdminMode)
 async def get_users_logs_data_base(message: types.Message):
-    DataBaseInfo = getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="logi")
+    DataBaseInfo = await getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="logi")
     await message.answer(DataBaseInfo)
     
 
 @dp.message_handler(commands=['adminOff'], state=StatesGroup.stateAdminMode)
 async def turn_admin_mode_off(message: types.Message, AdminState: FSMContext):
     await AdminState.finish()
+    await message.answer("exitAdminMode")
