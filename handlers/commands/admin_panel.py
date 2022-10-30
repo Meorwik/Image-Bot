3032cae74bdfbc9191be528b287c65474af54002
Meorwik <ilapolienko77@gmail.com>
2022-10-30 23:35:12 +0600

    fsdf

--- handlers/commands/admin_panel.py
+++ handlers/commands/admin_panel.py
@@ -10,9 +10,10 @@ async def set_admin_commands():
     await dp.bot.set_my_commands(
         [
             types.BotCommand("adminOn", "Запуска режима админа"),
-            types.BotCommand("Get_UsersDataBase", "База данных пользователей"),
-            types.BotCommand("Get_LogsDataBase", "Отображение доступных категорий")
-        ]
+            types.BotCommand("Users_db", "База данных пользователей"),
+            types.BotCommand("Logs_db", "Отображение доступных категорий"),
+            types.BotCommand("adminOff", "Выйти из режима админа")
+        ], scope=types.bot_command_scope.BotCommandScopeChat(ADMINS[0])
     )
 
 @dp.message_handler(commands=['adminOn'])
@@ -24,13 +25,13 @@ async def switch_admin_mode(message: types.Message):
     else: 
         await message.answer("У вас нет на это прав )")
         
-@dp.message_handler(commands=['Get_UsersDataBase'], state=StatesGroup.stateAdminMode)
+@dp.message_handler(commands=['Users_db'], state=StatesGroup.stateAdminMode)
 async def get_users_data_base(message: types.Message):
     DataBaseInfo = await getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="users")
     await message.answer(DataBaseInfo)
 
 
-@dp.message_handler(commands=['Get_LogsDataBase'], state=StatesGroup.stateAdminMode)
+@dp.message_handler(commands=['Logs_db'], state=StatesGroup.stateAdminMode)
 async def get_users_logs_data_base(message: types.Message):
     DataBaseInfo = await getting_info_from_the_same_databse(db_name="users_logs", get_what="*", get_from="logi")
     await message.answer(DataBaseInfo)
