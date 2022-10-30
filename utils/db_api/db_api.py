250ff70f45b24e691a8f806c25e8c40136791a74
Meorwik <ilapolienko77@gmail.com>
2022-10-30 18:00:26 +0600

    make it magically work

--- utils/db_api/db_api.py
+++ utils/db_api/db_api.py
@@ -1,5 +1,13 @@
 import sqlite3
 
+def check_uniq(funk):
+    async def inner_error_check(self):
+        try:
+            await funk()
+        except sqlite3.IntegrityError:
+            pass
+    return inner_error_check
+
 class DataBaseManager:
     async def connect(self, db_name):
         con = sqlite3.connect(f"data/DataBase/{db_name}.db")
@@ -14,14 +22,6 @@ class DataBaseManager:
     async def get_info(self, get_what, get_from):
         self.cursor.execute(f"SELECT {get_what} FROM {get_from}")
         return self.cursor.fetchall()
-    
-    async def check_uniq(funk):
-        async def inner_error_check(self):
-            try:
-                funk()
-            except sqlite3.IntegrityError:
-                pass
-        return inner_error_check
 
     @check_uniq
     async def add_new_info(self, add_where, add_what_info, exact_info):
