16544f68c2205aeffa80a2cba92689243dbb4237
Meorwik <ilapolienko77@gmail.com>
2022-10-31 21:01:11 +0600

    ;

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,6 @@
 from . import errors
 from . import commands
+from . import mein_bot_funk
+from . import special_folder
+from . import tool_panels
+from . import vars_for_handlers
--- handlers/commands/__init__.py
+++ handlers/commands/__init__.py
@@ -1,6 +1,5 @@
 from . import help
 from . import start
-from .mein_bot_funk import send_pictures
 from ..special_folder import for_her
 from . import category
 from ..tool_panels import admin_panel
