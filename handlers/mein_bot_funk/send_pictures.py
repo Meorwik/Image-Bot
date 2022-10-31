b419406b6e010154fdc07a90a7f2840181f519c9
Meorwik <ilapolienko77@gmail.com>
2022-10-31 23:04:06 +0600

    ds

--- handlers/mein_bot_funk/send_pictures.py
+++ handlers/mein_bot_funk/send_pictures.py
@@ -6,7 +6,7 @@ from keyboards.default.reply_keyboard import keyboard_continue_or_stop
 from LowLevelModuls.process_user_input import ParserManager
 from utils.parser.web_requests import CategoryDict
 from utils.db_api.db_api import DataBaseManager
-from commands.category import category_command_respond
+from ..commands.category import category_command_respond
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from ..vars_for_handlers.vars import *
