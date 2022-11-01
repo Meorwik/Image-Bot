c2344eff6fe31e817f1db50d00be45959242b2a6
Meorwik <ilapolienko77@gmail.com>
2022-11-01 23:17:32 +0600

    m

--- handlers/special_folder/for_her.py
+++ handlers/special_folder/for_her.py
@@ -31,7 +31,8 @@ async def result_of_special_command(msg: types.Message, state: FSMContext):
 async def some_spoecial_funk(message: types.Message):
     if await IS_ADMIN(message.chat.id):
         StatesGroup.stateWritingDownSomeInfo.set()
-        await message.answer("Ну что )\nПиши что хочкшь сказать )")
+        await message.answer("Ну что )\nПиши что хочeшь сказать )")
+        await write_file(msg=message, state=StatesGroup.stateWritingDownSomeInfo)
     else:
         pass
     
