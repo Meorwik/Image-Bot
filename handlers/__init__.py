207b1f4d065f1d35628463808ea99e33c86cbab7
Meorwik <ilapolienko77@gmail.com>
2022-11-17 22:31:01 +0600

    edrg

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 from . import errors
 from . import user_commands
 from . import mein_bot_funk
-from . import special_folder
 from . import tool_panels
 from . import vars_for_handlers
--- handlers/user_commands/__init__.py
+++ handlers/user_commands/__init__.py
@@ -1,5 +1,4 @@
 from . import help
 from . import start
-from ..special_folder import for_her
 from . import category
 from ..tool_panels import admin_panel
